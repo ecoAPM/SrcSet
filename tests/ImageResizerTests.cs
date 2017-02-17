667941a36ed029349a1d435c6cccf9b3f25e9a7b
Mark Daniel Osborne <markdosborne@gmail.com>
2017-02-17 16:40:19 +1100

    Add in xunit testing framework

---
+++ tests/ImageResizerTests.cs
@@ -0,0 +1,69 @@
+﻿using Xunit;
+
+
+namespace ImageResizerTests
+{
+    public class ResizerTests
+    {
+        [Fact]
+        public void PassingDummyTest()
+        {
+            Assert.Equal(2, Add(1, 1));
+        }
+ 
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+
+    public class SizeExtensionsTests
+    {
+        [Fact]
+        public void AspectRatioWorks()
+        {
+           
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
