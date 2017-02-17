b29ce11ad26e5a5ffbe1018f8f190b28f89089e3
Mark Daniel Osborne <markdosborne@gmail.com>
2017-02-17 16:59:06 +1100

    Create simple test for AspectRatio

--- tests/ImageResizerTests.cs
+++ tests/ImageResizerTests.cs
@@ -1,69 +1,33 @@
 ﻿using Xunit;
-
+using ImageResizer;
 
 namespace ImageResizerTests
 {
-    public class ResizerTests
-    {
-        [Fact]
-        public void PassingDummyTest()
-        {
-            Assert.Equal(2, Add(1, 1));
-        }
- 
-
-        int Add(int x, int y)
-        {
-            return x + y;
-        }
-
-        [Theory]
-        [InlineData(3)]
-        [InlineData(5)]
-        [InlineData(6)]
-        public void MyFirstTheory(int value)
-        {
-            Assert.True(IsOdd(value));
-        }
-
-        bool IsOdd(int value)
-        {
-            return value % 2 == 1;
-        }
-    }
-
     public class SizeExtensionsTests
     {
         [Fact]
-        public void AspectRatioWorks()
+        public void AspectRatioTest()
         {
-           
-            Assert.Equal(4, Add(2, 2));
+            ImageProcessorCore.Size size = new ImageProcessorCore.Size(50, 10);
+            double testAspectRatio = SizeExtensions.AspectRatio(size);
+            Assert.Equal(5, testAspectRatio );
         }
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
 
-        int Add(int x, int y)
-        {
-            return x + y;
-        }
+        /*  
+               [Theory]
+               [InlineData(3)]
+               [InlineData(5)]
+               [InlineData(6)]
+               public void MyFirstTheory(int value)
+               {
+                   Assert.True(IsOdd(value));
+               }
 
-        [Theory]
-        [InlineData(3)]
-        [InlineData(5)]
-        [InlineData(6)]
-        public void MyFirstTheory(int value)
-        {
-            Assert.True(IsOdd(value));
-        }
-
-        bool IsOdd(int value)
-        {
-            return value % 2 == 1;
-        }
+               bool IsOdd(int value)
+               {
+                   return value % 2 == 1;
+               }
+           */
     }
 }
