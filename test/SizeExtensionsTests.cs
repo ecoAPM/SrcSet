a02f8bb415620d16c4376969cd677f36dfc5fa60
Steve Desmond <steve@vtsv.ca>
2017-03-27 16:46:01 -0400

    add test project and simple test

---
+++ test/SizeExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using ImageSharp;
+using Xunit;
+
+namespace ImageResizer.Tests
+{
+    public class SizeExtensionsTests
+    {
+        [Fact]
+        public void AspectRatioIsCorrect()
+        {
+            //arrange
+            var size = new Size(3, 2);
+
+            //act
+            var aspectRatio = size.AspectRatio();
+
+            //assert
+            Assert.Equal(1.5, aspectRatio);
+        }
+    }
+}
