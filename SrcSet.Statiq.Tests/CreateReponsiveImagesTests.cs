2ef4d462c098c65356e416989d5ecb74cb5c5325
Steve Desmond <steve@vtsv.ca>
2021-10-25 13:49:14 -0400

    Use TestDocument instead of NSubstitute

--- SrcSet.Statiq.Tests/CreateReponsiveImagesTests.cs
+++ SrcSet.Statiq.Tests/CreateReponsiveImagesTests.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using NSubstitute;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using Statiq.Common;
@@ -17,23 +15,26 @@ namespace SrcSet.Statiq.Tests
 		public async Task ImageIsLoadedOnceAndResizedThrice()
 		{
 			//arrange
-			var doc = Substitute.For<IDocument>();
-			doc.ContentProvider.GetStream().Returns(new MemoryStream());
-			doc.Source.Returns(new NormalizedPath("/input/test.png", PathKind.Absolute));
-
+			var doc = new TestDocument(new NormalizedPath("input/test.png"));
 			var context = new TestExecutionContext();
 			context.SetInputs(doc);
 
-			var loader = Substitute.For<Func<Stream, Task<Image>>>();
-			loader.Invoke(Arg.Any<Stream>()).Returns(new Image<Rgba32>(4, 4));
+			Image image = new Image<Rgba32>(4, 4);
+			var loaded = 0;
 
-			var module = new CreateResponsiveImages(loader, new ushort[] { 1, 2, 3 });
+			Task<Image> Loader(Stream stream)
+			{
+				loaded++;
+				return Task.FromResult(image);
+			}
+
+			var module = new CreateResponsiveImages(Loader, new ushort[] { 1, 2, 3 });
 
 			//act
 			var docs = await module.ExecuteAsync(context);
 
 			//assert
-			await loader.Received(1).Invoke(Arg.Any<Stream>());
+			Assert.Equal(1, loaded);
 			Assert.Equal(3, docs.Count());
 		}
 	}
